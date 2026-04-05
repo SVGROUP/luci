a83bcadb3570d7a657ef233b538eb0c41d64b63f
SV <25300520+SaintVamp@users.noreply.github.com>
2026-04-05 22:36:07 +0800

    个性化-X86-25.12

---
+++ applications/luci-app-aliddns/root/usr/nginx/checkpppoe.sh
@@ -0,0 +1,15 @@
+REFUSED_NUM=$(nslookup 404.svsoft.fun | grep -c REFUSED)
+if [ $((REFUSED_NUM)) -gt 0 ]
+then
+    echo "restart wan"
+    `ifup wan`
+else
+    echo "DNS check passed"
+fi
+if ps w | grep -v grep | grep -q "nginx: master process"; then
+    echo "Nginx is running"
+else
+    echo "Nginx start"
+    uci set nginx.global.uci_enable='false'
+    /etc/init.d/nginx start
+fi
---
+++ applications/luci-app-aliddns/root/usr/nginx/init-route.sh
@@ -0,0 +1,3 @@
+chmod +x /etc/init.d/aliddns
+chmod +x /usr/bin/ethinfo
+echo "*/5 * * * * /bin/bash /usr/nginx/checkpppoe.sh"
